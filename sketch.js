16ce2de32f16cc319484c784f8d729071136dea0
Yunika Bajracharya <076bct095.yunika@pcampus.edu.np>
2022-02-05 19:17:19 +0545

    evaluate random neibour cell to visit

--- sketch.js
+++ sketch.js
@@ -2,11 +2,14 @@ let colsNum, rowsNum;
 let cellSize = 40;
 let grid = [];
 
+let currentCell;
+
 function setup() {
     let canvas = createCanvas(400, 400);
     canvas.position(150, 50);
     colsNum = floor(width / cellSize);
     rowsNum = floor(height / cellSize);
+    frameRate(5);
     
     for (let row = 0; row < rowsNum; row++) {
         for (let column = 0; column < colsNum; column++) {
@@ -14,6 +17,8 @@ function setup() {
             grid.push(cell);
         }
     }
+
+    currentCell = grid[0]; //starting from grid[0]
 }
 
 function draw() {
@@ -21,12 +26,56 @@ function draw() {
     for (let i = 0; i < grid.length; i++) {
         grid[i].show();
     }
+
+    currentCell.visited = true;
+    // randomly choosing one of the unvisited neighbours
+    let nextCell = currentCell.checkNeighbours();
+    // make the chosen cell the current cell and mark it as visited
+    if (nextCell) {
+        nextCell.visited = true;
+        currentCell = nextCell;
+    }
+}
+
+function index(row, column) {
+    if (row < 0 || column < 0 || column > colsNum - 1 || row > rowsNum - 1) {
+        return -1;
+    }
+    return column + row * colsNum;
 }
 
 function Cell(row, column) {
     this.row = row;
     this.column = column;
     this.walls = [true, true, true, true]; // top right bottom left
+    this.visited = false;
+
+    this.checkNeighbours = function() {
+        let neighbours = [];
+
+        let top    = grid[index(row - 1, column)];
+        let right  = grid[index(row, column + 1)];
+        let bottom = grid[index(row + 1, column)];
+        let left   = grid[index(row, column - 1)];
+
+        if (top && !top.visited) {
+            neighbours.push(top);
+        }
+        if (right && !right.visited) {
+            neighbours.push(right);
+        }
+        if (bottom && !bottom.visited) {
+            neighbours.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbours.push(left);
+        }
+
+        if (neighbours.length > 0) {
+            let r = floor(random(0, neighbours.length));
+            return neighbours[r];
+        }
+    }
 
     this.show = function() {
         let x = this.column * cellSize;
@@ -44,6 +93,11 @@ function Cell(row, column) {
         }
         if (this.walls[3]) {
             line(x, y, x, y + cellSize);
+        }
+
+        if (this.visited) {
+            fill(0, 255, 0, 100);
+            rect(x, y, cellSize, cellSize);
         }
     }
 }
