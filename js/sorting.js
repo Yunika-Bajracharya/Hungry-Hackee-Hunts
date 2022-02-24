f4722ccd696a57f545061cd5ec9ef1547b2e1ddf
Yunika Bajracharya <076bct095.yunika@pcampus.edu.np>
2022-02-24 10:01:53 +0545

    implemented dropdown menu

--- js/sorting.js
+++ js/sorting.js
@@ -1,24 +1,5 @@
 let sorted = false;
 
-//for drop down
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 var container = document.getElementById("array");
 
 let array = [];
