e320d4d0e9f9d314fcb22258eee94d699d504969
Reeya Baidya <reeya.baidya@gmail.com>
2022-03-01 02:10:31 +0545

    index added

--- js/selectionsort.js
+++ js/selectionsort.js
@@ -1,17 +1,17 @@
 async function SelectionSort(l, r, delay = 100) {
-  let bars = document.querySelectorAll(".block");
-  // Assign 0 to min_idx
-  var min_idx = l;
+  let blocks = document.querySelectorAll(".block");
+  // Assign 0 to max_idx
+  var max_idx = l;
   document.getElementsByClassName("range")[0].innerText = `[${l},${r}]`;
-  for (var i = 0; i < r + 1; i += 1) {
-    // Assign i to min_idx
-    min_idx = i;
+  for (var i = 14; i > 0 ; i --) {
+    // Assign i to max_idx
+    max_idx = i;
 
     // Provide color to the ith bar
-    bars[i].style.backgroundColor = "red";
-    for (var j = i + 1; j < bars.length; j += 1) {
+    blocks[i].style.backgroundColor = "red";
+    for (var j = 0 ; j <= i; j ++) {
       // Provide color to the jth bar
-      bars[j].style.backgroundColor = "yellow";
+      blocks[j].style.backgroundColor = "yellow";
 
       // To pause the execution of code for 300 milliseconds
       await new Promise((resolve) =>
@@ -21,31 +21,37 @@ async function SelectionSort(l, r, delay = 100) {
       );
 
       // To store the integer value of jth bar to var1
-      var val1 = parseInt(bars[j].childNodes[0].innerHTML);
+      var val1 = parseInt(blocks[j].childNodes[0].innerHTML);
 
-      // To store the integer value of (min_idx)th bar to var2
-      var val2 = parseInt(bars[min_idx].childNodes[0].innerHTML);
+      // To store the integer value of (max_idx)th bar to var2
+      var val2 = parseInt(blocks[max_idx].childNodes[0].innerHTML);
 
       // Compare val1 & val2
-      if (val1 < val2) {
-        if (min_idx !== i) {
-          // Provide  color to the (min-idx)th bar
-          bars[min_idx].style.backgroundColor = " pink";
+      if (val1 > val2) {
+        if (max_idx !== i) {
+          // Provide  color to the (max_idx)th bar
+          blocks[max_idx].style.backgroundColor = " pink";
         }
-        min_idx = j;
+        max_idx = j;
       } else {
         // Provide  color to the jth bar
-        bars[j].style.backgroundColor = " pink";
+        blocks[j].style.backgroundColor = " pink";
       }
+      document.getElementsByClassName("range")[0].innerText = `[${max_idx},${i}]`;
+      await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 300)
+    );
     }
 
-    // To swap ith and (min_idx)th bar
-    var temp1 = bars[min_idx].style.height;
-    var temp2 = bars[min_idx].childNodes[0].innerText;
-    bars[min_idx].style.height = bars[i].style.height;
-    bars[i].style.height = temp1;
-    bars[min_idx].childNodes[0].innerText = bars[i].childNodes[0].innerText;
-    bars[i].childNodes[0].innerText = temp2;
+    // To swap ith and (max_idx)th bar
+    var temp1 = blocks[max_idx].style.height;
+    var temp2 = blocks[max_idx].childNodes[0].innerText;
+    blocks[max_idx].style.height = blocks[i].style.height;
+    blocks[i].style.height = temp1;
+    blocks[max_idx].childNodes[0].innerText = blocks[i].childNodes[0].innerText;
+    blocks[i].childNodes[0].innerText = temp2;
 
     // To pause the execution of code for 300 milliseconds
     await new Promise((resolve) =>
@@ -54,12 +60,13 @@ async function SelectionSort(l, r, delay = 100) {
       }, 300)
     );
 
-    // Provide skyblue color to the (min-idx)th bar
-    bars[min_idx].style.backgroundColor = "pink";
+    // Provide color to the (min-idx)th bar
+    blocks[max_idx].style.backgroundColor = "pink";
 
-    // Provide lightgreen color to the ith bar
-    bars[i].style.backgroundColor = " #6b5b95";
+    // Provide color to the ith bar
+    blocks[i].style.backgroundColor = " #6b5b95";
   }
+  blocks[i].style.backgroundColor = " #6b5b95";
 }
 
 let selectionSortButton = document.getElementById("selectionSortButton");
